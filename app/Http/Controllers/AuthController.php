f8e08f12579efed5a1e75235e914e4c985dfec6a
slamerwin <slamerwin09@gmail.com>
2025-04-25 21:20:19 +0700

    update

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -15,24 +15,6 @@ class AuthController extends Controller
     //proses login
     public function prosesLogin(Request $request)
     {
-        //validasi data kosong
-        $data = $request->validate([
-            'username' => 'required',
-            'password' => 'required',
-        ]);
-
-        // panggil api via model
-        $response = LoginModel::auth($data);
-        // dd($response);
-        if ($response['success']) {
-            $request->session()->put("code", $request->username);
-            $request->session()->flash('status', true);
-            $request->session()->flash('message', 'Login Berhasil');
-            return redirect('/login');
-        } else {
-            $request->session()->flash('status', false);
-            $request->session()->flash('message', 'Username and Password not match');
-            return redirect('/login');
-        }
+      
     }
 }
--- app/Models/LoginModel.php
+++ app/Models/LoginModel.php
@@ -8,35 +8,5 @@ use Illuminate\Support\Facades\Http;
 
 class LoginModel extends Model
 {
-    use HasFactory;
-
-    static function path($param = null)
-    {
-        return "https://peta.umrah.ac.id/oauth" . $param;
-    }
-
-    static function auth($data)
-    {
-        try {
-            $response = Http::asForm()->post(static::path(), $data);
-
-            if ($response->successful()) {
-                return [
-                    'success' => true,
-                    'data' => $response->json(),
-                ];
-            }
-            return [
-                'success' => false,
-                'message' => 'Login gagal',
-                'status' => $response->status(),
-                'response' => $response->body(),
-            ];
-        } catch (\Exception $e) {
-            return [
-                'success' => false,
-                'message' => $e->getMessage(),
-            ];
-        }
-    }
+  
 }
